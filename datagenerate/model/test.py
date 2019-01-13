32b281e0033ad769730a6ea0a69c6be284893a8b
Chenzhoujia <33827855+Chenzhoujia@users.noreply.github.com>
2019-01-13 16:55:23 +0800

    Update test.py

--- datagenerate/model/test.py
+++ datagenerate/model/test.py
@@ -11,11 +11,12 @@ xyz_val_list3 = xyz_val_list3.reshape([-1,42])
 xyz_val_list4 = np.loadtxt("/home/chen/Documents/GNN_demo/train_values_all_4.txt")
 xyz_val_list4 = xyz_val_list4.reshape([-1,42])
 xyz_val_list = np.concatenate((xyz_val_list1, xyz_val_list2,xyz_val_list3,xyz_val_list4), axis=0)
-
+xyz_val_list_ori = np.loadtxt("/home/chen/Documents/denseReg-master/model/xyz_val_test_all.txt")
 meanJntError = []
 err_path = "/home/chen/Documents/denseReg-master/exp/train_cache/saved/GNN/_error.txt"
 for i in range(8252):
-    xyz_val = xyz_val_list[i,:]
+    #xyz_val = xyz_val_list[i,:]#图网络优化后的输出，误差test10.060924231540694mm
+    xyz_val = xyz_val_list_ori[i, :] #原始输出，误差10.233949127173911mm
     gt_val = xyz_gt_list[i,:]
     meanJntError.append(Evaluation.meanJntError(xyz_val, gt_val))
 Evaluation.plotError(meanJntError, err_path)
